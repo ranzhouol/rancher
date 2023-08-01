94a66f3aa1ce55b7303bbed87fbfee2b8c827327
冉州 <ranzhou@inspur.com>
2023-08-01 14:04:26 +0800

    feat: harbor代理-项目用户同步创建

--- pkg/k8sproxy/harborproxy/harborproxy.go
+++ pkg/k8sproxy/harborproxy/harborproxy.go
@@ -15,10 +15,26 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strconv"
 	"strings"
 )
 
+type proxyContext struct {
+	Context  *config.ScaledContext
+	Project  *harborproject.Project
+	UserInfo *userInfo
+}
+
+type userInfo struct {
+	authUsername string
+	authPassword string
+}
+
 func NewHarborProxy(context *config.ScaledContext) *httputil.ReverseProxy {
+	pc := &proxyContext{
+		Context: context,
+	}
+
 	targetUrl, err := url.Parse(pkg.HarborHost)
 	if err != nil {
 		return nil
@@ -28,13 +44,15 @@ func NewHarborProxy(context *config.ScaledContext) *httputil.ReverseProxy {
 	originalDirector := proxy.Director
 	proxy.Director = func(req *http.Request) {
 		originalDirector(req)
-		modifyRequest(context, req)
+		pc.modifyRequest(req)
 	}
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	proxy.Transport = tr
 	proxy.ErrorHandler = errorHandler()
+
+	proxy.ModifyResponse = pc.modifyResponse
 
 	return proxy
 }
@@ -46,10 +64,15 @@ func errorHandler() func(http.ResponseWriter, *http.Request, error) {
 	}
 }
 
-func modifyRequest(context *config.ScaledContext, req *http.Request) {
+func (p *proxyContext) modifyRequest(req *http.Request) {
 	//userid := req.Header.Get("Impersonate-User")
-	//harborUsername := req.Header.Get("Impersonate-Extra-Username")
+	//Username := req.Header.Get("Impersonate-Extra-Username")
+	context := p.Context
 	authUsername, authPassword, err := getUserInfo(context, req)
+	p.UserInfo = &userInfo{
+		authUsername: authUsername,
+		authPassword: authPassword,
+	}
 	if err != nil {
 		logrus.Errorf("制品库获取用户信息失败: %v", err.Error())
 	}
@@ -60,16 +83,58 @@ func modifyRequest(context *config.ScaledContext, req *http.Request) {
 	logrus.Infof("req.RequestURI =%s\n", req.RequestURI)
 	logrus.Infof("req.Method =%s\n", req.Method)
 
+	// 创建 harbor 项目
 	if req.Method == "POST" && req.RequestURI == "/harbor/api/v2.0/projects" {
 		// 使用 project-owner用户创建harbor项目
-		if err := createHarborProjectModifyHeader(req); err != nil {
+		if err := p.createHarborProjectModifyHeader(req); err != nil {
 			logrus.Errorf("创建制品库requset设置有误: %v", err.Error())
 		}
-		logrus.Info("req:\n", req)
+		//logrus.Info("req:\n", req)
 		return
 	}
 
 	client.RequestSetHeader(authUsername, authPassword, req)
+}
+
+func (p *proxyContext) modifyResponse(resp *http.Response) error {
+	logrus.Info("resp.Request.Method: ", resp.Request.Method)
+	logrus.Info("resp.Request.URL.Path: ", resp.Request.URL.Path)
+	if resp.Request.Method == "POST" && resp.Request.URL.Path == "/api/v2.0/projects" {
+		//logrus.Info("创建prject response:", resp)
+		//if resp.StatusCode == http.StatusCreated { //创建制品库成功
+		if err := syncProjectUserToHarbor(p, resp); err != nil {
+			logrus.Errorf("制品库用户同步失败:%v", err.Error())
+		}
+	}
+	//}
+	return nil
+}
+
+// 使用 project-owner用户创建harbor项目
+func (p *proxyContext) createHarborProjectModifyHeader(req *http.Request) error {
+	body, err := ioutil.ReadAll(req.Body)
+
+	if err != nil {
+		logrus.Error("读取响应内容失败:", err)
+		return err
+	}
+
+	var project *harborproject.Project
+	if err := json.Unmarshal(body, &project); err != nil {
+		logrus.Error("解析JSON失败: ", err.Error())
+		return err
+	}
+	logrus.Info(project.ProjectEdgesphereName)
+	p.Project = project
+
+	projectOwnerName := project.ProjectEdgesphereName + pkg.ProjectOwnerSuffix
+	projectOwnerPassword := pkg.MD5String(projectOwnerName)
+	client.RequestSetHeader(projectOwnerName, projectOwnerPassword, req)
+
+	req.ContentLength = int64(len(body))
+	req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+	return nil
 }
 
 // 获取用户名、密码
@@ -113,28 +178,47 @@ func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter,
 	}
 }
 
-// 使用 project-owner用户创建harbor项目
-func createHarborProjectModifyHeader(req *http.Request) error {
-	body, err := ioutil.ReadAll(req.Body)
+// 同步项目下的用户到harbor
+func syncProjectUserToHarbor(p *proxyContext, resp *http.Response) error {
+	logrus.Info("项目名:", p.Project.ProjectEdgesphereName)
+	logrus.Info("p.Project.ProjectEdgesphereId:", p.Project.ProjectEdgesphereId)
+	//p.Context.Management.Projects("").Get()
 
+	prtblist, err := p.Context.Management.ProjectRoleTemplateBindings(p.Project.ProjectEdgesphereId).List(v1.ListOptions{})
 	if err != nil {
-		logrus.Error("读取响应内容失败:", err)
 		return err
 	}
 
-	var project harborproject.Project
-	if err := json.Unmarshal(body, &project); err != nil {
-		logrus.Error("解析JSON失败: ", err.Error())
-		return err
+	for _, prtb := range prtblist.Items {
+		logrus.Info("prtb.Name名字: ", prtb.Name)
+		logrus.Info("prtb.Namespace名字: ", prtb.Namespace)
+		if prtb.Name == "creator-project-owner" {
+			logrus.Info("creator-project-owner名字: ", prtb.Name)
+			continue
+		}
+
+		userid := prtb.UserName
+		user, err := p.Context.Management.Users("").Get(userid, v1.GetOptions{})
+		if err != nil {
+			logrus.Errorf("failed to get user: %v, err: %v", userid, err.Error())
+			return err
+		}
+
+		// 创建harbor项目用户
+		projectName := p.Project.ProjectName
+		username := user.Username
+		// user.Description 对应于 authorityLeve
+		displayInt64, _ := strconv.ParseInt(user.Description, 10, 64)
+		logrus.Info("user.Description:", user.Description)
+		roleId := harborproject.AuthorityLeveToRoleId[displayInt64]
+		logrus.Info("roleID:", roleId)
+		if err := harborproject.CreateProjectMember(p.UserInfo.authUsername, p.UserInfo.authPassword, projectName, username, roleId); err != nil {
+			logrus.Errorf("创建制品库成员%v失败:%v", username, err.Error())
+			return err
+		}
 	}
-	logrus.Info(project.ProjectEdgesphereName)
 
-	projectOwnerName := project.ProjectEdgesphereName + pkg.ProjectOwnerSuffix
-	projectOwnerPassword := pkg.MD5String(projectOwnerName)
-	client.RequestSetHeader(projectOwnerName, projectOwnerPassword, req)
-
-	req.ContentLength = int64(len(body))
-	req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+	logrus.Info("制品库用户同步成功")
 
 	return nil
 }
--- pkg/k8sproxy/harborproxy/pkg/project/project.go
+++ pkg/k8sproxy/harborproxy/pkg/project/project.go
@@ -9,6 +9,12 @@ import (
 	"strings"
 )
 
+// edgesphere用户与harbor项目成员的对应关系
+var AuthorityLeveToRoleId = map[int64]int{
+	2: 1,
+	3: 2,
+}
+
 type Project struct {
 	ProjectName  string    `json:"project_name"`
 	MetaData     *metadata `json:"metadata"`
@@ -16,10 +22,21 @@ type Project struct {
 	RegistryId   int64     `json:"registry_id,omitempty"`
 	// add
 	ProjectEdgesphereName string `json:"project_edgesphere_name,omitempty"`
+	ProjectEdgesphereId   string `json:"project_edgesphere_id,omitempty"`
 }
 
 type metadata struct {
 	Public string `json:"public"`
+}
+
+// 项目成员
+type ProjectMember struct {
+	RoleId     int         `json:"role_id"`
+	MemberUser *memberUser `json:"member_user"`
+}
+
+type memberUser struct {
+	Username string `json:"username"`
 }
 
 func GetProject(authUsername, authPassword, projectNameOrid string) (*Project, error) {
@@ -60,6 +77,30 @@ func Create(authUsername, authPassword, projectName, public string, storageLimit
 
 	url := "/api/v2.0/projects"
 	err := client.PostClient(authUsername, authPassword, url, project)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 创建harbor项目成员
+func CreateProjectMember(authUsername, authPassword, projectName, username string, roleId int) error {
+	if authUsername == "admin" {
+		authPassword = pkg.HarborAdminPassword
+	}
+
+	member := &memberUser{
+		Username: username,
+	}
+
+	projectMember := &ProjectMember{
+		RoleId:     roleId,
+		MemberUser: member,
+	}
+
+	url := fmt.Sprintf("/api/v2.0/projects/%v/members", projectName)
+	err := client.PostClient(authUsername, authPassword, url, projectMember)
 	if err != nil {
 		return err
 	}
